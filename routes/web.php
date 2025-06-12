d63f56528402e1995096038db00697e2c5283a91
LuizPaluski <luizgustavodelimapalauski@gmail.com>
2025-06-12 11:52:40 -0300

    Adiconado registro de usuarios e listagem

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
+
+route::post("/users", [UserController::class,"store"])->name(name: "users.store");
+route::get("/users/create", action: [UserController::class,"create"])->name(name: "users.create");
+route::get("/users", action: [UserController::class,"index"])->name("users.index");
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
