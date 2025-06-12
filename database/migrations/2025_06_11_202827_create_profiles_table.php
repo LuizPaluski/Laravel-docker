3a3c5ac303315cc044347a442ccd3db22cf49ede
LuizPaluski <luizgustavodelimapalauski@gmail.com>
2025-06-12 08:46:16 -0300

    Adicionado chaves estrangeiras

--- database/migrations/2025_06_11_202827_create_profiles_table.php
+++ database/migrations/2025_06_11_202827_create_profiles_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
+            $table->foreignId(column: "user_id")
+            ->constrained()
+            ->onDelete("cascade");
             $table->timestamps();
         });
     }
